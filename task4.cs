f8ba036391f34ac25772687b6293d53d59e32292
yagutrslbyl <yaguter-bpa202@code.edu.az>
2025-10-07 05:50:48 +0400

    task4

---
+++ task4.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("eded daxil et");
+int n = Convert.ToInt32(Console.ReadLine());
+int s = 0;
+while (n>0)
+
+{
+  s = s + 1;
+  n = n / 10;
+
+}
+
+Console.WriteLine("en boyuk mertebe: " + Math.Pow(10, s-1));
