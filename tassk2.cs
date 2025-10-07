6e7903038b8114d20fc6330bd60ce5751efed1bb
yagutrslbyl <yaguter-bpa202@code.edu.az>
2025-10-07 05:38:21 +0400

    task2

---
+++ tassk2.cs
@@ -0,0 +1,13 @@
+﻿int x = 144;
+int y = 5;
+int max = 0;
+for(int i=0;i<=x;i++)
+{
+    if (i % y == 0)
+    {
+        max = i;
+    }
+}
+Console.WriteLine(max);
+if (y>x)
+    Console.WriteLine("-1");
