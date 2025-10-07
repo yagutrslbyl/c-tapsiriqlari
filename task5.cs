6df162fd4c09529613064630d4f74b1d66ad52cc
yagutrslbyl <yaguter-bpa202@code.edu.az>
2025-10-07 05:55:23 +0400

    task5

---
+++ task5.cs
@@ -0,0 +1,34 @@
+﻿
+
+int month = 11;
+switch(month)
+{
+   case 1: Console.WriteLine("Qis");
+       break;
+   case 2: Console.WriteLine("Qis");
+       break;
+   case 3: Console.WriteLine("Yaz");
+       break;
+   case 4: Console.WriteLine("Yaz");
+       break;
+   case 5: Console.WriteLine("Yaz");
+       break;
+   case 6: Console.WriteLine("Yay");
+       break;
+   case 7: Console.WriteLine("Yay");
+      break;
+   case 8: Console.WriteLine("Yay");
+      break;
+   case 9: Console.WriteLine("Payiz");
+      break;
+   case 10: Console.WriteLine("Payiz");
+      break;
+   case 11: Console.WriteLine("Payiz");
+       break;
+   case 12: Console.WriteLine("Qis");
+       break;
+   default:
+      Console.WriteLine("Bele bir ay yoxdur");
+       break;
+
+}      
